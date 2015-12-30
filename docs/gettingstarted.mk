3f0f7a525c4c22da07c239497bad8410e79e3a99
Bob Gradeck <rgradeck@gmail.com>
2015-12-30 17:28:45 -0500

    Update getting started

--- docs/gettingstarted.mk
+++ docs/gettingstarted.mk
@@ -8,5 +8,19 @@ department list
 
 #Eligibility
 -Government, Academic, and Nonprofit organizations are eligible to share data on the Western Pennsylvania Regional Data Center's open data portal.
+-Once an organization becomes a publisher on the Data Center, an "organization" page will be created on the Data Center Website. <LINK>
+
+#Checklist
+-Before a new publisher can share information through the Regional Data Center, the following steps must be completed.
+-1. Data Deposit Agreement
+-2. Training
+-3. Logo 
+-4. Organizational description
+-5. (Optional) department listing
+
+
 
 #Data Deposit Agreement
+-In order to share data with the Data Center, a Data Deposit Agreement will need to be completed and returned. 
+-Electronic submissions of the agreement are encouraged. A scanned version of the document can be emailed to the Data Center <mailto: www.wprdc.org>. The completed agreement can be included as an email attachment.
+-The person signing the Data Deposit Agreement as a representative of the publishing organization must have the authority to commit the publishing organization into an agreement with the University of Pittsburgh.
