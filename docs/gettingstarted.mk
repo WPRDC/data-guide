ec88e93c5410e0c63356ab7dba32c21d953207b9
Bob Gradeck <rgradeck@gmail.com>
2015-12-30 17:11:24 -0500

    Create gettingstarted.mk

---
+++ docs/gettingstarted.mk
@@ -0,0 +1,12 @@
+
+data deposit guide - signature and responsibilities
+organized as publishing "classes"
+training
+logo
+description (include url)
+department list
+
+#Eligibility
+-Government, Academic, and Nonprofit organizations are eligible to share data on the Western Pennsylvania Regional Data Center's open data portal.
+
+#Data Deposit Agreement
